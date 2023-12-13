056d6723631a3f0dee6c355fb41dfc253d7fa415
matt <leem@voldemort.local>
2023-12-13 16:39:37 +0100

    added density and updated labels for others

--- R/mr_plots.R
+++ R/mr_plots.R
@@ -80,3 +80,162 @@ mr_scatter_plot <- function (mr_results, dat)
 }
 
 
+#' Forest plot
+#'
+#' @param singlesnp_results from [mr_singlesnp()].
+#' @param exponentiate Plot on exponential scale. The default is `FALSE`.
+#'
+#' @export
+#' @return List of plots
+mr_forest_plot <- function(singlesnp_results, exponentiate=FALSE)
+{
+  res <- plyr::dlply(singlesnp_results, c("id.exposure", "id.outcome"), function(d)
+  {
+    d <- plyr::mutate(d)
+    if(sum(!grepl("All", d$SNP)) < 2) {
+      return(
+        blank_plot("Insufficient number of SNPs")
+      )
+    }
+    levels(d$SNP)[levels(d$SNP) == "All - Inverse variance weighted"] <- "All - IVW"
+    levels(d$SNP)[levels(d$SNP) == "All - MR Egger"] <- "All - Egger"
+    am <- grep("All", d$SNP, value=TRUE)
+    d$up <- d$b + 1.96 * d$se
+    d$lo <- d$b - 1.96 * d$se
+    d$tot <- 0.01
+    d$tot[d$SNP %in% am] <- 1
+    d$SNP <- as.character(d$SNP)
+    nom <- d$SNP[! d$SNP %in% am]
+    nom <- nom[order(d$b)]
+    d <- rbind(d, d[nrow(d),])
+    d$SNP[nrow(d)-1] <- ""
+    d$b[nrow(d)-1] <- NA
+    d$up[nrow(d)-1] <- NA
+    d$lo[nrow(d)-1] <- NA
+    d$SNP <- ordered(d$SNP, levels=c(am, "", nom))
+
+    xint <- 0
+    if(exponentiate)
+    {
+      d$b <- exp(d$b)
+      d$up <- exp(d$up)
+      d$lo <- exp(d$lo)
+      xint <- 1
+    }
+
+    ggplot2::ggplot(d, ggplot2::aes(y=SNP, x=b)) +
+      ggplot2::geom_vline(xintercept=xint, linetype="dotted") +
+      # ggplot2::geom_errorbarh(ggplot2::aes(xmin=pmax(lo, min(d$b, na.rm=T)), xmax=pmin(up, max(d$b, na.rm=T)), size=as.factor(tot), colour=as.factor(tot)), height=0) +
+      ggplot2::geom_errorbarh(ggplot2::aes(xmin=lo, xmax=up, size=as.factor(tot), colour=as.factor(tot)), height=0) +
+      ggplot2::geom_point(ggplot2::aes(colour=as.factor(tot))) +
+      ggplot2::geom_hline(ggplot2::aes(yintercept = which(levels(SNP) %in% "")), colour="grey") +
+      ggplot2::scale_colour_manual(values=c("black", "red")) +
+      ggplot2::scale_size_manual(values=c(0.3, 1)) +
+      # xlim(c(min(c(0, d$b), na.rm=T), max(c(0, d$b), na.rm=T))) +
+      ggplot2::theme(
+        legend.position="none",
+        axis.text.y=ggplot2::element_text(size=8),
+        axis.ticks.y=ggplot2::element_line(size=0),
+        axis.title.x=ggplot2::element_text(size=8)) +
+      ggplot2::labs(y="", x=paste0("MR effect size for", d$id.exposure[1], "' on '", d$id.outcome[1], "'"))
+  })
+  res
+}
+
+
+#' Plot results from leaveoneout analysis
+#'
+#' Plot results from leaveoneout analysis.
+#'
+#' @param leaveoneout_results Output from [mr_leaveoneout()].
+#'
+#' @export
+#' @return List of plots
+mr_leaveoneout_plot <- function(leaveoneout_results)
+{
+  res <- plyr::dlply(leaveoneout_results, c("id.exposure", "id.outcome"), function(d)
+  {
+    d <- plyr::mutate(d)
+    # Need to have at least 3 SNPs because IVW etc methods can't be performed with fewer than 2 SNPs
+    if(sum(!grepl("All", d$SNP)) < 3) {
+      return(
+        blank_plot("Insufficient number of SNPs")
+      )
+    }
+    d$up <- d$b + 1.96 * d$se
+    d$lo <- d$b - 1.96 * d$se
+    d$tot <- 1
+    d$tot[d$SNP != "All"] <- 0.01
+    d$SNP <- as.character(d$SNP)
+    nom <- d$SNP[d$SNP != "All"]
+    nom <- nom[order(d$b)]
+    d <- rbind(d, d[nrow(d),])
+    d$SNP[nrow(d)-1] <- ""
+    d$b[nrow(d)-1] <- NA
+    d$up[nrow(d)-1] <- NA
+    d$lo[nrow(d)-1] <- NA
+    d$SNP <- ordered(d$SNP, levels=c("All", "", nom))
+
+    ggplot2::ggplot(d, ggplot2::aes(y=SNP, x=b)) +
+      ggplot2::geom_vline(xintercept=0, linetype="dotted") +
+      # ggplot2::geom_errorbarh(ggplot2::aes(xmin=pmax(lo, min(d$b, na.rm=T)), xmax=pmin(up, max(d$b, na.rm=T)), size=as.factor(tot), colour=as.factor(tot)), height=0) +
+      ggplot2::geom_errorbarh(ggplot2::aes(xmin=lo, xmax=up, size=as.factor(tot), colour=as.factor(tot)), height=0) +
+      ggplot2::geom_point(ggplot2::aes(colour=as.factor(tot))) +
+      ggplot2::geom_hline(ggplot2::aes(yintercept = which(levels(SNP) %in% "")), colour="grey") +
+      ggplot2::scale_colour_manual(values=c("black", "red")) +
+      ggplot2::scale_size_manual(values=c(0.3, 1)) +
+      # xlim(c(min(c(0, d$b), na.rm=T), max(c(0, d$b), na.rm=T))) +
+      ggplot2::theme(
+        legend.position="none",
+        axis.text.y=ggplot2::element_text(size=8),
+        axis.ticks.y=ggplot2::element_line(size=0),
+        axis.title.x=ggplot2::element_text(size=8)) +
+      ggplot2::labs(y="", x=paste0("MR leave-one-out sensitivity analysis for'", d$id.exposure[1], "' on '", d$id.outcome[1], "'"))
+  })
+  res
+}
+
+
+#' Density plot
+#'
+#' @param singlesnp_results from [mr_singlesnp()].
+#' @param mr_results Results from [mr()].
+#' @param exponentiate Plot on exponentiated scale. The default is `FALSE`.
+#' @param bandwidth Density bandwidth parameter.
+#'
+#' @export
+#' @return List of plots
+mr_density_plot <- function(singlesnp_results, mr_results, exponentiate=FALSE, bandwidth="nrd0")
+{
+  res <- plyr::dlply(singlesnp_results, c("id.exposure", "id.outcome"), function(d)
+  {
+    d <- plyr::mutate(d)
+    if(sum(!grepl("All", d$SNP)) < 2) {
+      return(
+        blank_plot("Insufficient number of SNPs")
+      )
+    }
+    d$SNP <- as.character(d$SNP)
+
+    d2 <- subset(d, !grepl("All - ", SNP))
+    d1 <- subset(mr_results, id.exposure == d2$id.exposure[1] & id.outcome == d2$id.outcome[1])
+
+    xint <- 0
+    if(exponentiate)
+    {
+      d$b <- exp(d$b)
+      d$up <- exp(d$up)
+      d$lo <- exp(d$lo)
+      xint <- 1
+    }
+
+    ggplot2::ggplot(d2, ggplot2::aes(x=b)) +
+      ggplot2::geom_vline(xintercept=xint, linetype="dotted") +
+      ggplot2::geom_density(ggplot2::aes(weight=1/se), bw=bandwidth) +
+      ggplot2::geom_point(y=0, colour="red", ggplot2::aes(size=1/se)) +
+      ggplot2::geom_vline(data=mr_results, ggplot2::aes(xintercept=b, colour=method)) +
+      ggplot2::scale_colour_brewer(type="qual") +
+      ggplot2::labs(x = paste0("Per SNP MR estimate of", d$id.exposure[1], "' on '", d$id.outcome[1]))
+  })
+  res
+}
