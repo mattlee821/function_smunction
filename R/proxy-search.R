e951df3e8921f5364f232b42d0e6dce532eb119f
matt <leem@voldemort.local>
2023-12-20 09:12:46 +0100

    add dplyr calls

--- R/proxy-search.R
+++ R/proxy-search.R
@@ -1,14 +1,21 @@
 #' @title proxy_search: search for proxies when using read_outcome_data()
+#' @description
+#' This function gets proxy SNPs for MR analyses when using local outcome data.
+#' It takes formatted exposure and outcome data from `TwoSampleMR`, searches for
+#' missing SNPs in the outcome, identifies which of these missing-SNPs are
+#' available in the provided reference panel, extracts all proxy-SNPs for the
+#' missing-SNPs from the reference panel, returns a data frame with the top
+#' proxy-SNP for each missing-SNP. You MUST have a local reference panel. Only
+#' works with rsID.
 #' @return a data frame
 #' @param data_exposure exposure data frame
 #' @param data_outcome outcome data frame
-#' @param data_outcome_path file path used for read_outcome_data()
-#' @param data_reference reference data; bim file (i.e., paste(data_reference_path, ".bim"))
+#' @param data_outcome_path file path used for `read_outcome_data()`
+#' @param data_reference reference data; bim file
 #' @param data_reference_path file path for your downloaded reference panel
-#' @param tag_r2 r2 for proxy SNP; from get_ld_proxies(); default = 0.8
-#' @param tag_kb window to look for proxy SNPs; from get_ld_proxies(); default = 5000
-#' @param tag_nsnp from get_ld_proxies(); default = 5000
-
+#' @param tag_r2 r2 for proxy SNP; from `get_ld_proxies()`; default = 0.8
+#' @param tag_kb window to look for proxy SNPs; from `get_ld_proxies()`; default = 5000
+#' @param tag_nsnp from `get_ld_proxies()`; default = 5000
 proxy_search <- function(data_exposure, data_outcome, data_outcome_path, data_reference, data_reference_path,
                          tag_r2 = 0.8, tag_kb = 5000, tag_nsnp = 5000) {
 
@@ -48,23 +55,23 @@ proxy_search <- function(data_exposure, data_outcome, data_outcome_path, data_re
                                        out = tempfile())
   ## format proxy data: change column order and names, add proxy.outcome = TRUE
   proxies <- proxies %>%
-    select(target_snp.outcome = SNP_A,
+    dplyr::select(target_snp.outcome = SNP_A,
            proxy_snp.outcome = SNP_B,
            target_a1.outcome = A1,
            target_a2.outcome = A2,
            proxy_a1.outcome = B1,
            proxy_a2.outcome = B2,
            R) %>%
-    mutate(proxy.outcome = TRUE,
+    dplyr::mutate(proxy.outcome = TRUE,
            SNP = proxy_snp.outcome) %>%
-    select(proxy.outcome, everything())
+    dplyr::select(proxy.outcome, everything())
   message(paste0("## proxy-SNP(s) for ", length(unique(proxies$target_snp.outcome)), " missing-SNP(s) found; ", "proxy-SNP(s) for ", length(unique(as.factor(snps_reference))) - length(unique(as.factor(proxies$target_snp.outcome))), " missing-SNP(s) not available (e.g., no proxy-SNP or r2 < provided)"))
 
   # extract proxies from outcome ====
   message(paste0("# 3. extracting proxy-SNP(s) from outcome"))
   proxy_snps <- proxies %>% # select unique proxy SNPs to extract
-    distinct(proxy_snp.outcome) %>%
-    pull(proxy_snp.outcome)
+    dplyr::distinct(proxy_snp.outcome) %>%
+    dplyr::pull(proxy_snp.outcome)
   data_outcome_proxies <- TwoSampleMR::read_outcome_data(filename = data_outcome_path,
                                                          snps = proxy_snps,
                                                          sep = "\t",
@@ -80,18 +87,18 @@ proxy_search <- function(data_exposure, data_outcome, data_outcome_path, data_re
                                                          id_col = "phenotype",
                                                          chr_col = "CHR",
                                                          pos_col = "POS")
-  data_outcome_proxies <- left_join(data_outcome_proxies, proxies, by = c("SNP" = "SNP"))
+  data_outcome_proxies <- dplyr::left_join(data_outcome_proxies, proxies, by = c("SNP" = "SNP"))
   message(paste0("## proxy-SNP(s) for ", length(unique(as.factor(data_outcome_proxies$target_snp.outcome))), " of ", length(unique(as.factor(proxies$target_snp.outcome))), " missing-SNP(s) extracted"))
 
   # select proxy-SNP(s) with the highest R2 ====
   data_outcome_proxies <- data_outcome_proxies %>%
-    group_by(target_snp.outcome) %>%
-    filter(R == max(R)) %>%
-    slice(1) %>%
-    select(-R)
+    dplyr::group_by(target_snp.outcome) %>%
+    dplyr::filter(R == max(R)) %>%
+    dplyr::slice(1) %>%
+    dplyr::select(-R)
 
   ## Bind rows of data_outcome with data_outcome_proxies
-  data_outcome <- bind_rows(data_outcome, data_outcome_proxies)
+  data_outcome <- dplyr::bind_rows(data_outcome, data_outcome_proxies)
 
   return(data_outcome)
 }
