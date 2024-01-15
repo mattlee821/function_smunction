76b7d311e0952fe601e2218e8e859ffa19774afd
matt <leem@voldemort.local>
2024-01-15 13:30:10 +0100

    updated with new pal

--- R/palette.R
+++ R/palette.R
@@ -3,6 +3,12 @@
 #' @description assign palette to use palette_discrete <- palette()
 #'
 palette <- function() {
-  palette_discrete <- wesanderson::wes_palette(names(wes_palettes)[8], type = "discrete")
-  return(palette_discrete)
+  palette_discrete1 <- wesanderson::wes_palette(name = "Royal1")
+  palette_discrete2 <- wesanderson::wes_palette(name = "Darjeeling1")
+  palette_continuous <- structure(.Data = grDevices::colorRampPalette(c(wesanderson::wes_palette(name = "Royal1")[1],
+                                                                        wesanderson::wes_palette(name = "Royal1")[2]))(100),
+                                  class = "palette", name = "Royal1 continuous")
+  return(palette_discrete1)
+  return(palette_discrete2)
+  return(palette_continuous)
 }
