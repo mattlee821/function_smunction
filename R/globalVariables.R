6252f005b5ef64d829a0535f34e903703c273e3d
matt <leem@iarc.who.int>
2024-07-30 12:13:42 +0200

    global vars upload

---
+++ R/globalVariables.R
@@ -0,0 +1,16 @@
+# R/globalVariables.R
+utils::globalVariables(c(
+  "a", "b", "id.exposure", "mean_f_stats", "SNP", "r2", "samplesize.exposure", "k",
+  "fstat_from_r2", "SNP.PP.H4", "position", "y", "snp", "SNP_causal_exposure", "name",
+  "value", "gene", "pvalue", "ID", "log10(p-adjust)", "dist", "hclust", "Term", "Cancer",
+  "Count", "tiff", "dev.off", "cncr_mal_clrt", "cncr_mal_clrt_colon", "cncr_mal_clrt_rectum",
+  "ageevent", "age", "cvd_t2d_coh", "coxph", "indevent", "..i", "mr_keep.outcome",
+  "check_units", "create_ids", "mr_keep", "beta.exposure", "beta.outcome", "se.outcome",
+  "se.exposure", "a", "b", "label", "logp", "logp1", "logp2", "rsid", "sex", "tiff",
+  "dev.off", "setNames", "sd", "prcomp", "normalization_residualMixedModels", "Predictor",
+  "MAF", "effect_allele.outcome", "A1", "other_allele.outcome", "A2", "mr_keep",
+  "proxy.outcome", "proxy_snp.outcome", "target_snp.outcome", "mean_value", "x", "P", "id.outcome",
+  "P", "id.outcome", "spread", "x", "SNP_A", "pos", "se",
+  "lo", "up", "tot", "B1", "B2", "R", "SNP_B", "expected", "observed",
+  "cupper", "clower", ".", "IdentifierPipeline"
+))
