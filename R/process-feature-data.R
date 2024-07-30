7f8d5854b4a2512d61c3cb83b6c52f8d65e70093
matt <leem@iarc.who.int>
2024-07-30 11:14:18 +0200

    include normalization functions

--- R/process-feature-data.R
+++ R/process-feature-data.R
@@ -51,7 +51,7 @@
 #' @import cowplot
 #' @import parallel
 #' @export
-process_feature_data <- function(
+process_data <- function(
     data,
     data_meta_features,
     data_meta_samples,
@@ -106,8 +106,7 @@ process_feature_data <- function(
     ## remove features with >X% missing
     id_features <- df_features %>%
       filter(.data[[col_missing]] > VAR_missing_pct) %>%
-      pull(col_features) %>%
-      gsub(",", ".", .)
+      pull(col_features)
 
     df <- df %>%
       select(-all_of(id_features))
@@ -275,8 +274,8 @@ process_feature_data <- function(
     data_features_analysis <- as.data.frame(pca_features$x)
 
     # step 2: sample outliers ====
-    llof <- bigutilsr::LOF(data_samples_analysis) # compute distances using local outlier factor
-    outliers_samples <- which(llof > bigutilsr::tukey_mc_up(llof)) # identify outlier threshold using tukeys rule
+    llof <- LOF(data_samples_analysis) # compute distances using local outlier factor
+    outliers_samples <- which(llof > tukey_mc_up(llof)) # identify outlier threshold using tukeys rule
     id_samples_exclude <- rownames(data_samples_analysis[outliers_samples, ])
     # plot PCs with lof highlighting
     cat("making sample outlier plot \n")
@@ -289,7 +288,7 @@ process_feature_data <- function(
                                     mapping = aes(color = llof),
                                     legend = 11) +
       scale_color_viridis_c(direction = -1, option = "F") +
-      labs(color = paste("Local outlier factor:", round(bigutilsr::tukey_mc_up(llof), 2)), title = paste0("Samples to exclude: ",  length(outliers_samples))) +
+      labs(color = paste("Local outlier factor:", round(tukey_mc_up(llof), 2)), title = paste0("Samples to exclude: ",  length(outliers_samples))) +
       theme_minimal() +
       theme(strip.background = element_rect(fill = "grey")) +
       theme(legend.position = "bottom") +
@@ -297,8 +296,8 @@ process_feature_data <- function(
 
 
     # step 3: feature outliers ====
-    llof <- bigutilsr::LOF(data_features_analysis) # compute distances using local outlier factor
-    outliers_features <- which(llof > bigutilsr::tukey_mc_up(llof)) # identify outlier threshold using tukeys rule
+    llof <- LOF(data_features_analysis) # compute distances using local outlier factor
+    outliers_features <- which(llof > tukey_mc_up(llof)) # identify outlier threshold using tukeys rule
     id_features_exclude <- rownames(data_features_analysis[outliers_features, ])
     # plot PCs with lof highlighting
     cat("making feature outlier plot \n")
@@ -311,7 +310,7 @@ process_feature_data <- function(
                                      mapping = aes(color = llof),
                                      legend = 11) +
       scale_color_viridis_c(direction = -1, option = "F") +
-      labs(color = paste("Local outlier factor:", round(bigutilsr::tukey_mc_up(llof), 2)), title = paste0("Features to exclude: ",  length(outliers_features))) +
+      labs(color = paste("Local outlier factor:", round(tukey_mc_up(llof), 2)), title = paste0("Features to exclude: ",  length(outliers_features))) +
       theme_minimal() +
       theme(strip.background = element_rect(fill = "grey")) +
       theme(legend.position = "bottom") +
@@ -320,19 +319,19 @@ process_feature_data <- function(
     # step 4: exclusion ====
     ## save figure
     cat("saving outlier plots \n")
-    tiff(paste0(path_outliers, "plot-outliers_", LABEL, ".tiff"), width = 1000, height = 1000, units = "px")
-    print(cowplot::plot_grid(
-      GGally::ggmatrix_gtable(plot_samples),
-      GGally::ggmatrix_gtable(plot_features),
-      nrow = 2))
-    dev.off()
+    # tiff(paste0(path_outliers, "plot-outliers_", LABEL, ".tiff"), width = 1000, height = 1000, units = "px")
+    # print(cowplot::plot_grid(
+    #   GGally::ggmatrix_gtable(plot_samples),
+    #   GGally::ggmatrix_gtable(plot_features),
+    #   nrow = 2))
+    # dev.off()
     ## make exclusions
     if(length(outliers_samples) > 0) {df <- df[-outliers_samples, ]}
     if(length(outliers_features) > 0) {df <- df[, -outliers_features]}
     ## save excluded IDs
     cat("saving outlier info \n")
     id_outliers <- list(features_extreme = id_features, features_lof = id_features_exclude, samples_lof = id_samples_exclude)
-    saveRDS(object = id_outliers, file = paste0(path_outliers, "id-outliers_", LABEL, ".rds"))
+    # saveRDS(object = id_outliers, file = paste0(path_outliers, "id-outliers_", LABEL, ".rds"))
   }
 
   # case-control data ====
@@ -365,9 +364,9 @@ process_feature_data <- function(
     df <- df %>%
       rownames_to_column(var = col_samples)
     ## make df
-    df <- list(data.metabo = df,
-               others = df_samples,
-               aux = df_features %>%
+    df <- list(data_features = df,
+               data_samples = df_samples,
+               data_meta_features = df_features %>%
                  rename(Name = all_of(col_features)))
     ## transformation
     df <- normalization_residualMixedModels(df,
@@ -386,3 +385,179 @@ process_feature_data <- function(
   return(df)
 }
 
+#' Normalize feature Data Using Residual Mixed Models
+#'
+#' This function normalizes feature data by removing unwanted effects using mixed models.
+#' It accounts for random effects and fixed effects specified by the user, and optionally
+#' corrects for heteroskedasticity.
+#'
+#' @param list A list containing:
+#'   \item{data_features}{A data frame or tibble of dimensions n x (K+p) with:
+#'     \describe{
+#'       \item{n}{Number of observations.}
+#'       \item{p}{Number of features}
+#'       \item{K}{Number of variables used for unique identification of individuals.}
+#'     }}
+#'   \item{data_samples}{A data frame or tibble of dimensions n x (K+d) with:
+#'     \describe{
+#'       \item{n}{Number of observations.}
+#'       \item{K}{Number of unique identifiers.}
+#'       \item{d}{Additional variables useful in final analysis (e.g., country, age, BMI).}
+#'     }}
+#'   \item{data_meta_features}{A p x 3 matrix indicating each features Name, Class, and Type.}
+#' @param forIdentifier A character vector of strings indicating the names of variables used for unique identification of individuals.
+#' @param listRandom A character vector of strings containing variable names modeled as random effects to be removed. If not NULL, should be either of length 1 or contain nested variables.
+#' @param listFixedToKeep A character vector of strings containing variable names modeled as fixed effects to be kept.
+#' @param listFixedToRemove A character vector of strings containing variable names modeled as fixed effects to be removed.
+#' @param HeteroSked A string or NULL. If not NULL, the name of the variable for which heteroskedasticity will be accounted for. Must be included in `listRandom`.
+#'
+#' @return A list with:
+#'   \item{data}{A tibble with unwanted variation removed.}
+#'   \item{data_samples}{The input 'data_samples' data frame, ordered by IdentifierPipeline.}
+#'   \item{data_meta_features}{The input 'data_meta_features' matrix.}
+#'
+#' @import tidyverse reshape2 hglm nlme lme4
+#' @export
+normalization_residualMixedModels <- function(list,
+                                              forIdentifier = c("Study", "Batch", "CaseSet", "Idepic"),
+                                              listRandom = NULL,
+                                              listFixedToKeep = NULL,
+                                              listFixedToRemove = NULL,
+                                              HeteroSked = NULL) {
+
+  # Convert list of data frames to tibble and unite identifiers ====
+  data_features <- as_tibble(list$data_features) %>%
+    unite(IdentifierPipeline, all_of(forIdentifier), sep = "", remove = FALSE) %>%
+    arrange(IdentifierPipeline)
+  data_samples <- as_tibble(list$data_samples) %>%
+    unite(IdentifierPipeline, all_of(forIdentifier), sep = "", remove = FALSE) %>%
+    arrange(IdentifierPipeline)
+  data_meta_features <- as_tibble(list$data_meta_features)
+
+  # Check for consistency between data_features and data_samples ====
+  if (sum(data_features$IdentifierPipeline == data_samples$IdentifierPipeline) < nrow(data_features)) {
+    stop("Idepic should be the same in data_features and data_samples")
+  }
+
+  # Merge data_features with data_samples and select relevant columns ====
+  var.context <- unique(c("IdentifierPipeline", forIdentifier, listRandom, listFixedToKeep, listFixedToRemove))
+  data <- left_join(data_features,
+                    data_samples %>%
+                      dplyr::select(all_of(var.context)) %>%
+                      dplyr::select(-any_of(forIdentifier)),
+                    by = "IdentifierPipeline") %>%
+    dplyr::select(all_of(var.context), all_of(colnames(list$data_features)[which(colnames(list$data_features) %in% data_meta_features$Name)]))
+
+  # Prepare data for modeling
+  data <- reshape2::melt(data,
+                         id.vars = which(colnames(data) %in% var.context),
+                         value.name = "Y",
+                         variable.name = "id_feature")
+  data <- data %>%
+    arrange(across(c("id_feature", all_of(var.context))))
+  data.context <- data[1:table(data$id_feature)[[1]], 1:length(var.context)]
+
+  # Remove unwanted effects using mixed models
+  id_feature <- unique(data$id_feature)
+  for (i in id_feature) {
+    cat(i, "\n")
+    df <- data %>%
+      filter(id_feature == i)
+    res <- FUNnormalization_residualMixedModels(df = df,
+                                                listRandom = listRandom,
+                                                listFixedToKeep = listFixedToKeep,
+                                                listFixedToRemove = listFixedToRemove,
+                                                HeteroSked = HeteroSked,
+                                                i = i)
+    data.context <- cbind(data.context, res)
+  }
+
+  # Finalize output
+  colnames(data.context) <- c(var.context, as.character(id_feature))
+  data.context <- as_tibble(data.context) %>%
+    arrange(IdentifierPipeline) %>%
+    dplyr::select(-IdentifierPipeline)
+  data_samples <- data_samples %>%
+    arrange(IdentifierPipeline) %>%
+    dplyr::select(-IdentifierPipeline)
+
+  return(list(data = data.context, data_samples = data_samples, data_meta_features = data_meta_features))
+}
+
+#' Compute Residuals Using Mixed Models
+#'
+#' This function calculates residuals from a mixed model to remove unwanted effects.
+#'
+#' @param df A data frame with feature measurements and relevant variables.
+#' @param listRandom A character vector of strings specifying random effect variables.
+#' @param listFixedToKeep A character vector of strings specifying fixed effect variables to keep.
+#' @param listFixedToRemove A character vector of strings specifying fixed effect variables to remove.
+#' @param HeteroSked A string or NULL. If not NULL, specifies the variable for heteroskedasticity correction.
+#' @param i A string specifying the current feature being processed.
+#'
+#' @return A numeric vector of residuals for the given feature.
+#'
+#' @import lme4 nlme
+#' @export
+FUNnormalization_residualMixedModels <- function(df,
+                                                 listRandom,
+                                                 listFixedToKeep,
+                                                 listFixedToRemove,
+                                                 HeteroSked,
+                                                 i) {
+
+  listFixedInit <- c(listFixedToKeep, listFixedToRemove)
+  listFixed <- if (is.null(listFixedInit)) "1" else listFixedInit
+  y <- df$Y
+
+  # Prepare data for model fitting
+  df_lmer0 <- cbind.data.frame(y = y, df[, c(listRandom, listFixedInit)])
+  colnames(df_lmer0)[-1] <- c(listRandom, listFixedInit)
+  indNoNAs <- which(rowSums(is.na(df_lmer0)) == 0)
+  df_lmer <- df_lmer0[indNoNAs, ]
+  Y1 <- rep(NA, length(y))
+
+  if (!is.null(listFixedToKeep)) {
+    df_temp <- data.frame(df_lmer[, listFixedToKeep])
+    colnames(df_temp) <- listFixedToKeep
+    XFixedToKeep <- model.matrix(~ ., data = df_temp)
+  }
+
+  if (is.null(HeteroSked)) {
+    # Fit mixed model without heteroskedasticity correction
+    textformulaMixed <- paste("y ~", paste0(listFixed, collapse = "+"),
+                              paste0(" + (1|", paste0(listRandom, collapse = ") + (1|"), ")"))
+    tochecklmer <- tryCatch(lmer(as.formula(textformulaMixed), data = df_lmer,
+                                 control = lmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 2e5))),
+                            error = function(e) NULL)
+    if (!is.null(tochecklmer)) {
+      if (!is.null(listFixedToKeep)) {
+        Y1[indNoNAs] <- XFixedToKeep %*% matrix(fixef(tochecklmer)[names(fixef(tochecklmer)) %in% colnames(XFixedToKeep)], ncol = 1) + resid(tochecklmer)
+      } else {
+        Y1[indNoNAs] <- mean(y) + resid(tochecklmer)
+      }
+    }
+  } else {
+    # Fit mixed model with heteroskedasticity correction
+    listrandomeffect <- lapply(listRandom, function(x) as.formula(paste0("~1|", x)))
+    forweights <- as.formula(paste0("~1|", HeteroSked))
+    textformulaFixedHeteroSked <- paste("y ~", paste0(listFixed, collapse = "+"))
+
+    tochecklme <- tryCatch(lme(as.formula(textformulaFixedHeteroSked), random = listrandomeffect, weights = varIdent(form = forweights),
+                               data = df_lmer,
+                               control = list(maxIter = 900, msMaxIter = 900, niterEM = 900, msMaxEval = 900, opt = "optim")),
+                           error = function(e) NULL)
+
+    if (!is.null(tochecklme)) {
+      forrescale <- sd(residuals(tochecklme, type = "pearson")) / sd(residuals(tochecklme))
+      if (!is.null(listFixedToKeep)) {
+        Y1[indNoNAs] <- XFixedToKeep %*% matrix(fixed.effects(tochecklme)[names(fixed.effects(tochecklme)) %in% colnames(XFixedToKeep)], ncol = 1) +
+          residuals(tochecklme, type = "pearson") / forrescale
+      } else {
+        Y1[indNoNAs] <- mean(y) + residuals(tochecklme, type = "pearson") / forrescale
+      }
+    }
+  }
+
+  return(Y1)
+}
