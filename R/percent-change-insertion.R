b6c72615d81f2ac680b78fbe2dd74130bc25f9cb
matt <leem@iarc.who.int>
2024-11-10 10:53:05 +0100

    description update

--- R/percent-change-insertion.R
+++ R/percent-change-insertion.R
@@ -1,14 +1,10 @@
-# function to calculate percent change between corresponding cells in two dataframes and insert these as new columns
-
-#' @title precent change and insertion
-#' @description You need two data frames of equal size. The function assumes column 1 is an unused ID column.
-#' @param data1 your original dataframe
-#' @param data2 your second dataframe
-#' @param col_name the name of the column to calculate percent change for
-#' @returns a dataframe
-#' @note
-#' for (col_name in colnames(data2)) {
-#'   data2 <- calculate_and_insert_percent_change(data1, data2, col_name)}
+#' @title Percent Change and Insertion
+#' @description This function calculates the percent change between corresponding columns in two dataframes and inserts these changes as new columns in the second dataframe. It assumes that the first column in each dataframe is an unused ID column.
+#' @param data1 The original dataframe.
+#' @param data2 The second dataframe to which the percent change columns will be added.
+#' @param col_name The name of the column (as a character string) to calculate the percent change for.
+#' @returns A dataframe with the new percent change columns added to `data2`.
+#' @note Both `data1` and `data2` must have the same structure and number of rows. Column 1 should be excluded from calculations.
 #' @export
 calculate_and_insert_percent_change <- function(data1, data2, col_name) {
   new_col_name <- paste0(col_name, "_change")
