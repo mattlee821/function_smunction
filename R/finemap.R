d875111f36684b15e805959d3fafd3aa13520982
matt <leem@iarc.who.int>
2024-12-13 19:37:31 +0100

    resgionplot

--- R/finemap.R
+++ R/finemap.R
@@ -87,14 +87,14 @@ finemap_pval_LD <- function(df, bfile, pval_threshold = 5E-8, clump_r2 = 0.001) 
 susieR_cs_table <- function(susieR_model, df) {
   # Extract the credible sets
   cs_list <- susieR_model$sets$cs
-  pip <- data.frame(SNP = names(susieR_model$pip), pip_value = susieR_model$pip)
+  PIP <- data.frame(SNP = names(susieR_model$pip), pip_value = susieR_model$pip)
 
   table <- data.frame(
     SNP = df$SNP,
     POS = df$POS,
     P = df$P
   ) %>%
-    left_join(pip, by = "SNP")
+    left_join(PIP, by = "SNP")
 
   # Initialize the `cs` column to store credible set labels
   table$cs <- NA
@@ -128,7 +128,8 @@ susieR_cs_table <- function(susieR_model, df) {
     dplyr::group_by(cs) %>%
     dplyr::mutate(label = if_else(P == min(P), SNP, NA_character_)) %>%  # Identify SNP with smallest p-value
     dplyr::ungroup() %>%
-    dplyr::bind_rows(table %>% dplyr::filter(is.na(cs))) # Add back rows with NA cs
+    dplyr::bind_rows(table %>% dplyr::filter(is.na(cs))) %>% # Add back rows with NA cs
+    dplyr::mutate(test = "SuSiE")
 
   # Return the final table
   return(table)
@@ -202,9 +203,176 @@ finimom_cs_table <- function(finimom_model, df) {
     dplyr::group_by(cs) %>%
     dplyr::mutate(label = if_else(P == min(P), SNP, NA_character_)) %>%  # Identify SNP with smallest p-value
     dplyr::ungroup() %>%
-    dplyr::bind_rows(table %>% dplyr::filter(is.na(cs))) # Add back rows with NA cs
+    dplyr::bind_rows(table %>% dplyr::filter(is.na(cs))) %>% # Add back rows with NA cs
+    dplyr::mutate(test = "Finimom")
 
   # Return the final table
   return(table)
 }
 
+#' Generate a Regional Association Plot with Recombination Rates and Genes
+#'
+#' This function creates a regional association plot showing -log10(p-values)
+#' for SNPs, overlaid with recombination rates and gene annotations. Points can be
+#' highlighted and labeled based on the SNP of interest.
+#'
+#' @param df Dataframe containing GWAS summary statistics.
+#' @param rsid Column name for SNP identifiers in `df`.
+#' @param chrom Column name for chromosome numbers in `df`.
+#' @param pos Column name for SNP positions (in base pairs) in `df`.
+#' @param p_value Column name for p-values in `df`.
+#' @param label Optional vector of SNP IDs (rsIDs) to label in the plot. Default is NULL.
+#' @param trait Column name for trait or phenotype in `df`. Default is NULL.
+#' @param plot_pvalue_threshold Minimum p-value threshold for points to be plotted. Default is 0.1.
+#' @param genome_build Genome build version for gene and recombination rate annotations. Default is "GRCh38".
+#' @param population Population for recombination rate data (e.g., "EUR"). Default is "EUR".
+#' @param plot_title Title of the plot. Default is NULL.
+#' @param plot_subtitle Subtitle of the plot. Default is NULL.
+#'
+#' @return A combined `ggplot2` object with the regional association plot and gene annotations.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' gg_regionplot(df = gwas_data, rsid = "rsid", chrom = "CHR", pos = "POS",
+#'               p_value = "P", label = c("rs12345"), plot_title = "Region Plot")
+#' }
+gg_regionplot <- function(df,
+                          rsid,
+                          chrom,
+                          pos,
+                          p_value,
+                          label = NULL,
+                          trait = NULL,
+                          plot_pvalue_threshold = 0.1,
+                          genome_build = "GRCh38",
+                          population = "EUR",
+                          plot_title = NULL,
+                          plot_subtitle = NULL) {
+
+  # Process and clean input dataframe ====
+  df <- df %>%
+    mutate(log10_pval = -log10(df[[p_value]])) %>%  # Compute -log10(p-value)
+    dplyr::select(rsid = SNP, chromosome = CHR, position = pos, ref = EA, alt = OA, log10_pval, trait = phenotype) %>%
+    dplyr::mutate(dplyr::across(where(is.factor), as.character)) %>%  # Convert factor columns to characters
+    dplyr::mutate(ref = stringr::str_to_upper(ref), alt = stringr::str_to_upper(alt)) %>%  # Normalize allele case
+    dplyr::group_by(trait, rsid) %>%
+    dplyr::slice_max(log10_pval) %>%  # Keep the SNP with the highest log10(p-value) per trait
+    dplyr::ungroup() %>%
+    drop_na()
+
+  # Extract recombination rate data ====
+  ylim <- max(df$log10_pval, na.rm = TRUE) + 0.3 * max(df$log10_pval, na.rm = TRUE)  # Adjust y-axis limit
+  recomb_df <- recomb_extract_locuszoom(
+    chrom = unique(df$chromosome),
+    start = min(df$position) - 1,
+    end = max(df$position) + 1,
+    genome_build = genome_build
+  ) %>%
+    select(position, recomb_rate)
+
+  # Generate the regional association plot ====
+  plot_region <- df %>%
+    distinct(rsid, .keep_all = TRUE) %>%
+    filter(log10_pval > -log10(plot_pvalue_threshold)) %>%
+    mutate(
+      size = ifelse(rsid %in% label, 6, 4),   # Larger size for labeled points
+      color = ifelse(rsid %in% label, "purple", "black"),  # Purple color for labeled points
+      alpha = ifelse(rsid %in% label, 1, 0.7),  # Higher opacity for labeled points
+      shape = ifelse(rsid %in% label, 18, 16)  # Different shape for labeled points
+    ) %>%
+    ggplot(aes(
+      x = position / 1e6,  # Convert position to Mb
+      y = log10_pval)) +
+
+    # Add recombination rate line (background layer)
+    geom_line(
+      data = recomb_df,
+      mapping = aes(
+        x = position / 1e6,
+        y = recomb_rate),
+      color = "lightblue",
+      linewidth = 0.5
+    ) +
+
+    # Plot non-label points
+    geom_point(data = subset(df, !rsid %in% label),
+               aes(size = 4, color = "darkgrey", alpha = 0.7, shape = 16)) +
+
+    # Plot label points
+    geom_point(data = subset(df, rsid %in% label),
+               aes(size = 6, color = "purple", alpha = 1, shape = 18)) +
+
+    # Set scales for aesthetic mappings
+    scale_size_identity() +
+    scale_color_identity() +
+    scale_alpha_identity() +
+    scale_shape_identity() +
+
+    # Add labels and theme
+    labs(
+      title = plot_title,
+      subtitle = plot_subtitle,
+      x = "Position (Mb)",
+      y = "-log10(P)") +
+    scale_x_continuous(labels = scales::comma) +
+    cowplot::theme_cowplot() +
+
+    # Add point labels
+    ggrepel::geom_label_repel(
+      data = subset(df, rsid %in% label),
+      aes(label = rsid),
+      size = 4,
+      color = "black",
+      fontface = "bold",
+      fill = "white",
+      min.segment.length = 0,
+      box.padding = 1,
+      alpha = 1,
+      max.overlaps = 100,
+      force = 10
+    ) +
+
+    # Add horizontal significance threshold line
+    geom_hline(yintercept = -log10(5E-8), linetype = "dashed")
+
+  # Add recombination rate as a secondary axis
+  plot_region <- plot_region +
+    scale_y_continuous(
+      name = "-log10(P)",
+      limits = c(0, ylim),
+      sec.axis = sec_axis(
+        ~ . * (100 / ylim),  # Scale secondary axis
+        name = "Recombination rate (cM/Mb)"
+      )
+    ) +
+    theme(axis.title.y.right = element_text(vjust = 1.5))
+
+  # Generate the gene plot ====
+  plot_gene <- gg_geneplot(
+    chr = unique(df$chromosome),
+    start = min(df$position) - 1,
+    end = max(df$position) + 1,
+    genome_build = genome_build
+  ) +
+    theme(plot.margin = margin(0, 5.5, 5.5, 5.5))
+
+  # Combine the region and gene plots ====
+  plot_region <- patchwork::wrap_plots(list(
+    plot_region +
+      labs(x = "") +
+      theme(
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_blank(),
+        plot.margin = margin(5.5, 5.5, 0, 5.5)
+      ),
+    plot_gene
+  ),
+  nrow = 2,
+  heights = c(3, 1)
+  )
+
+  # Return the combined plot ====
+  return(plot_region)
+}
